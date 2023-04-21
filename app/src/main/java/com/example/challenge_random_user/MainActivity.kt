00b9682b7fce6fd09f0b721afeb45365352635c7
scerdan <sjcerdansantiagojoel@teco.com.ar>
2023-04-21 11:43:35 -0300

    Base Project

---
+++ app/src/main/java/com/example/challenge_random_user/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.challenge_random_user
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.challenge_random_user.ui.theme.Challenge_random_userTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Challenge_random_userTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Challenge_random_userTheme {
+        Greeting("Android")
+    }
+}
