db68006ccb4d5704bfbf3367d646b757a05698db
scerdan <sjcerdansantiagojoel@teco.com.ar>
2023-04-21 20:25:32 -0300

    Charged picture

--- app/src/main/java/com/example/challenge_random_user/MainActivity.kt
+++ app/src/main/java/com/example/challenge_random_user/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.challenge_random_user
 
-import android.content.ClipData.Item
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -10,17 +8,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.annotation.ExperimentalCoilApi
@@ -28,7 +25,6 @@ import coil.compose.rememberImagePainter
 import com.example.challenge_random_user.presentation.viewmodels.UserViewModel
 import com.example.challenge_random_user.ui.theme.Challenge_random_userTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -54,11 +50,10 @@ class MainActivity : ComponentActivity() {
 fun Greeting(vModel: UserViewModel = hiltViewModel()) {
     val viewModel by vModel.state.collectAsState()
     val userList = viewModel.allUsers
-    val myFavourites = userList.size
 
 
     LazyColumn(Modifier.fillMaxSize(1f)){
-        items(myFavourites) {ite ->
+        items(userList) {item ->
             Card(
                 Modifier
                     .fillMaxWidth(1f)
@@ -66,7 +61,14 @@ fun Greeting(vModel: UserViewModel = hiltViewModel()) {
                     .padding(top = 10.dp)
                     .background(Color.Gray)
             ) {
-
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16f / 9f),
+                    painter = rememberImagePainter(data = item.picture.large),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth
+                )
             }
         }
     }
